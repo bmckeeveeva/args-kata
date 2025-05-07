2b508256dd4acb88aac494dbbc469849b88c0f5b
Bob McKee <bob.mckee@veeva.com>
2025-05-07 09:43:30 -0400

    ArgsParser cleanup - removed State parameter so State member was used rather than passed around

--- java/src/main/java/args/before/ArgsParser.java
+++ java/src/main/java/args/before/ArgsParser.java
@@ -17,33 +17,33 @@ public class ArgsParser {
       return state;
     }
 
-    parseArguments(state);
+    parseArguments();
 
     return state;
   }
 
-  private void parseArguments(State state) throws ArgsException {
+  private void parseArguments() throws ArgsException {
     for (state.currentArgument = 0; state.currentArgument < state.args.length; state.currentArgument++) {
       String arg = state.args[state.currentArgument];
-      parseArgument(state, arg);
+      parseArgument(arg);
     }
   }
 
-  private void parseArgument(State state, String arg) throws ArgsException {
+  private void parseArgument(String arg) throws ArgsException {
     if (!arg.startsWith("-")) {
       throw Util.createArgsError(String.format("'%s' is not a valid argument format.", arg),
           ArgsErrorCode.INVALID_ARGUMENT_FORMAT);
     }
-    parseElements(state, arg);
+    parseElements(arg);
   }
 
-  private void parseElements(State state, String arg) throws ArgsException {
+  private void parseElements(String arg) throws ArgsException {
     for (int i = 1; i < arg.length(); i++) {
-      setArgument(state, arg.charAt(i));
+      setArgument(arg.charAt(i));
     }
   }
 
-  private void setArgument(State state, char argChar) throws ArgsException {
+  private void setArgument(char argChar) throws ArgsException {
     for (Handler handler : Handler.HANDLERS) {
       if (handler.isSupportedArg(state, argChar)) {
         handler.setArg(state, argChar);
